46555fb32ea6c4fd4dd3b5768755c59d93020353
Morten Ydefeldt <morten@ydefeldt.com>
2014-02-25 13:03:44 +0100

    Initial Commit

---
+++ cluedo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cluedo
+//
+//  Created by Morten Ydefeldt on 25/02/14.
+//  Copyright (c) 2014 Ydefeldt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ cluedo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  cluedo
+//
+//  Created by Morten Ydefeldt on 25/02/14.
+//  Copyright (c) 2014 Ydefeldt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
